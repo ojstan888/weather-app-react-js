459e892cbd1313d0cba949173d7617e7d9998282
stanislav <stas.oykin@yandex.ru>
2020-06-02 17:40:57 +0300

    deploying

--- src/components/weather.js
+++ src/components/weather.js
@@ -2,13 +2,14 @@ import React from "react";
 
 const Weather = props => (
     <div>
-         { props.city && 
-            <div className="infoWeath">
+         { props.city 
+            ? <div className="infoWeath">
                 <p className="infoWeath__b">Местоположение: <span>{props.city}, {props.country}</span></p>
                 <p className="infoWeath__b">Температура: <span>{props.temp}</span></p>
                 <p className="infoWeath__b">Давление: <span>{props.pressure}</span></p>
                 <p className="infoWeath__b">Закат солнца: <span>{props.sunset}</span></p>
             </div>
+            : null
             }
             <p className="error">{props.error}</p>
         </div>
